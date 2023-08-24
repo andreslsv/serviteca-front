2e530996f2f6aa0e4ee1d556726c913afb3dec79
andreslsv <fasalasdsgn@gmail.com>
2023-08-24 14:14:47 -0500

    Agregar apiService basico

---
+++ src/app/servicios/api.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private _httpClient: HttpClient,) { }
+  
+  getQuery(_nodo:string,_nombreQuery:string,_paramsQuery:string){
+    const nombreQuery = _nombreQuery;
+    const paramsQuery = _paramsQuery?`?${_paramsQuery}`:``;
+    return this._httpClient.get(`${environment.apiUrl}/${nombreQuery}${paramsQuery}`);
+  }
+
+  postQuery(_nodo:string,_nombreQuery:string,_paramsQuery:string,_dataQuery:any){
+    const nombreQuery = _nombreQuery;
+    const paramsQuery = _paramsQuery?`?${_paramsQuery}`:``;
+    return this._httpClient.post(`${environment.apiUrl}/${nombreQuery}${paramsQuery}`,_dataQuery);
+  }
+
+  putQuery(){}
+  deleteQuery(){}
+}
